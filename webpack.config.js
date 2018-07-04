9c7affcead34a997b8014273be06d1b74a31bd9d
chevion <chevionlu@gmail.com>
2018-07-04 12:03:02 +0800

    Update Compress

--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,7 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   // module.exports.devtool = '#source-map'
+  module.exports.devtool = false
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
@@ -79,7 +80,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new UglifyJSPlugin({
-      sourceMap: true
+      sourceMap: false
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
