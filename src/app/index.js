85a6be48e3e325889a05d84445f36e487828ac7a
WangJie <i@i8e.net>
2019-04-07 01:41:23 +0800

    chore: upgrade vuetify-loader (1.2.1)

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import logger from '../common/logger'
 import Vuetify from 'vuetify/lib'
-import { Scroll } from 'vuetify/lib/directives'
 import VueClipboard from 'vue-clipboard2'
 import colors from 'vuetify/es5/util/colors'
 import 'vuetify/src/stylus/app.styl'
@@ -18,9 +17,6 @@ Vue.use(Vuetify, {
   theme: {
     primary: colors.lightBlue,
   },
-  directives: {
-    Scroll, // TODO: remove this after the version of vuetify-loader containing https://github.com/vuetifyjs/vuetify-loader/pull/38 be published
-  }
 })
 
 const app = new Vue({
